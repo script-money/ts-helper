d54f8ea67d89b3f01192d5270f6cc01b8e31b2c8
脚本刷钱 <hello@script.money>
2021-02-09 17:30:21 +0800

    :bug: add log when query_all_moment

--- query_all_moment.py
+++ query_all_moment.py
@@ -1,4 +1,10 @@
 import asyncio
 from request import get_all_play_info
+from loggers import setup_logging_pre
+import sys
 
-asyncio.run(get_all_play_info())
+try:
+    setup_logging_pre()
+    asyncio.run(get_all_play_info())
+except KeyboardInterrupt:
+    sys.exit(0)
